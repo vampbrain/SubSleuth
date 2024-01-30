0d35305325425f343d0bcfc8434515b7d0d8ebe0
Yogesh-005 <117334483+Yogesh-005@users.noreply.github.com>
2024-01-30 12:12:01 +0530

    popup

--- popup.js
+++ popup.js
@@ -1,12 +1,32 @@
-
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('analyzeButton').addEventListener('click', analyzeLinks);
+  document.getElementById('copyUrlButton').addEventListener('click', copyUrl); // Adding event listener for the copyUrl function
 });
 
 function analyzeLinks() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, { action: 'analyzeLinks' });
+  });
+}
+
+function copyUrl() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var activeTab = tabs[0];
+    var url = activeTab.url;
+
+    // Retrieve existing URLs from storage
+    chrome.storage.local.get({ urls: [] }, function (data) {
+      var urls = data.urls;
+
+      // Add the current URL to the list
+      urls.push(url);
+
+      // Save the updated list back to storage
+      chrome.storage.local.set({ urls: urls }, function () {
+        console.log('URL added:', url);
+      });
+    });
   });
 }
 
